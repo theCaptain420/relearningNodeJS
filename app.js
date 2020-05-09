7c47a1d75e3b595cd33b03242894b33a8d8909f4
thecaptain420 <mads.nielsen@hotmail.com>
2020-05-09 19:38:45 +0200

    server is running

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-function sayHello(name){
-    console.log("hello "+ name)
-}
+const http = require('http');
 
-sayHello("mads")
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
